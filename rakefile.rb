7935e925a798bf6691987c736b900c3898e223ea
Dima K <dima@groupon.com>
2011-04-30 13:57:16 -0400

    Adding a rake file to execute tests in cloud

--- rakefile.rb
+++ rakefile.rb
@@ -12,7 +12,17 @@ namespace :selenium do
 
   desc 'Run the current selenium test in cloud (pass in tag to run only that tag)'
   task :sauce_selenium do
+    ENV['SAUCE_OS']              = ENV['SAUCE_OS']              ? ENV['SAUCE_OS']              : "Windows 2003"
+    ENV['SAUCE_BROWSER']         = ENV['SAUCE_BROWSER']         ? ENV['SAUCE_BROWSER']         : "iehta"
+    ENV['SAUCE_BROWSER_VERSION'] = ENV['SAUCE_BROWSER_VERSION'] ? ENV['SAUCE_BROWSER_VERSION'] : "7."
+
+    ENV['SAUCE'] = "true"
     
+    Cucumber::Rake::Task.new(:selenium_in_cloud ) do |t|
+      t.profile = "sauce"
+    end
+
+    Rake::Task[:selenium_in_cloud].invoke
   end
 end
 
