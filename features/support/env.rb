93da325c9cd3885b8a415719b5cbcbb2deaa0599
Dima K <dima@groupon.com>
2011-04-30 13:59:22 -0400

    Adding configs for sauce in the env.rb file

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,3 +5,46 @@ require 'capybara/cucumber'
 require 'capybara/spec/test_app'
 
 Capybara.current_driver = :selenium
+
+if ENV['SAUCE']
+  require 'sauce'
+  require 'sauce/capybara'
+
+  ENV['SAUCE_USERNAME'] = "username"
+  ENV['SAUCE_API_KEY'] = "your-api-key-goes-here"
+
+  Sauce.config do |c|
+    c.username = ENV['SAUCE_USERNAME']
+    c.access_key = ENV['SAUCE_API_KEY']    
+    
+    c.browsers = [
+      [ENV['SAUCE_OS'], ENV['SAUCE_BROWSER'], ENV['SAUCE_BROWSER_VERSION']]
+    ]
+  end
+  
+  puts "[Connecting to Sauce OnDemand...]"
+  config = Sauce::Config.new
+  ENV['SAUCE_CAPYBARA_DOMAIN'] = "#{rand(10000)}.groupon.com"
+  @sauce_tunnel = Sauce::Connect.new(:host => "127.0.0.1",
+                                     :port => Capybara.server_port,
+                                     :domain => ENV['SAUCE_CAPYBARA_DOMAIN'],
+                                     :quiet => true)
+  @sauce_tunnel.wait_until_ready
+  puts "[Connected to Sauce OnDemand...]"
+  
+  at_exit do
+    @sauce_tunnel.disconnect
+  end
+
+  Before('@javascript') do
+    Capybara.current_driver = :sauce
+  end
+  
+  Before do |scenario|
+    
+    ENV['SAUCE_CAPYBARA_NEW_SESSION'] = "true"
+    ENV['SAUCE_CAPYBARA_JOB_NAME'] = scenario.name
+    
+  end
+  
+end
